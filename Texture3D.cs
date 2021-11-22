7f979d1327d3165a62720f1fff30e266eb6d844e
Stephan Meesters <stephan.meesters@gmail.com>
2021-11-22 18:42:41 +0100

    first commit

---
+++ Texture3D.cs
@@ -0,0 +1,74 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using PixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat;
+using System;
+using itk.simple;
+
+namespace LearnOpenTK.Common
+{
+    // A helper class, much like Shader, meant to simplify loading textures.
+    public class Texture3D
+    {
+        public readonly int Handle;
+
+        public static Texture3D LoadFromFile(string path)
+        {
+            // Generate handle
+            int handle = GL.GenTexture();
+
+            // Bind the handle
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture3D, handle);
+            
+
+            ImageFileReader reader = new ImageFileReader();
+            reader.SetFileName(path);
+            Image image = reader.Execute();
+
+            var buffer = image.GetBufferAsUInt16();
+            var size = image.GetSize();
+            uint xs = size[0];
+            uint ys = size[1];
+            uint zs = size[2];
+            //unsafe
+           // {
+           //     byte* src = (byte*)buffer.ToPointer();
+            //}
+
+            GL.TexImage3D(TextureTarget.Texture3D,
+                    0,
+                    PixelInternalFormat.R16ui,
+                    (int)xs,
+                    (int)ys,
+                    (int)zs,
+                    0,
+                    PixelFormat.Red,
+                    PixelType.UnsignedShort,
+                    buffer);
+
+            GL.TexParameter(TextureTarget.Texture3D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture3D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            GL.TexParameter(TextureTarget.Texture3D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture3D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture3D);
+
+            return new Texture3D(handle);
+        }
+
+        public Texture3D(int glHandle)
+        {
+            Handle = glHandle;
+        }
+
+        // Activate texture
+        // Multiple textures can be bound, if your shader needs more than just one.
+        // If you want to do that, use GL.ActiveTexture to set which slot GL.BindTexture binds to.
+        // The OpenGL standard requires that there be at least 16, but there can be more depending on your graphics card.
+        public void Use(TextureUnit unit)
+        {
+            GL.ActiveTexture(unit);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,100 @@
+﻿using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+
+namespace LearnOpenTK
+{
+    // Be warned, there is a LOT of stuff here. It might seem complicated, but just take it slow and you'll be fine.
+    // OpenGL's initial hurdle is quite large, but once you get past that, things will start making more sense.
+    public class Window : GameWindow
+    {
+        // Create the vertices for our triangle. These are listed in normalized device coordinates (NDC)
+        // In NDC, (0, 0) is the center of the screen.
+        // Negative X coordinates move to the left, positive X move to the right.
+        // Negative Y coordinates move to the bottom, positive Y move to the top.
+        // OpenGL only supports rendering in 3D, so to create a flat triangle, the Z coordinate will be kept as 0.
+        private readonly float[] _vertices =
+        {
+            -0.5f, -0.5f, 0.0f, // Bottom-left vertex
+             0.5f, -0.5f, 0.0f, // Bottom-right vertex
+             0.0f,  0.5f, 0.0f  // Top vertex
+        };
+
+        // These are the handles to OpenGL objects. A handle is an integer representing where the object lives on the
+        // graphics card. Consider them sort of like a pointer; we can't do anything with them directly, but we can
+        // send them to OpenGL functions that need them.
+
+        // What these objects are will be explained in OnLoad.
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        // This class is a wrapper around a shader, which helps us manage it.
+        // The shader class's code is in the Common project.
+        // What shaders are and what they're used for will be explained later in this tutorial.
+        private Shader _shader;
+
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        // Now, we start initializing OpenGL.
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            Texture3D tex = Texture3D.LoadFromFile("F:/Medical3DApplication/App/MedViewer/Data/ct.nii.gz");
+        }
+
+        // Now that initialization is done, let's create our render loop.
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+           
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            var input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            // When the window gets resized, we have to call GL.Viewport to resize OpenGL's viewport to match the new size.
+            // If we don't, the NDC will no longer be correct.
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        protected override void OnUnload()
+        {
+            // Unbind all the resources by binding the targets to 0/null.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Delete all the resources.
+            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteVertexArray(_vertexArrayObject);
+
+            GL.DeleteProgram(_shader.Handle);
+
+            base.OnUnload();
+        }
+    }
+}
